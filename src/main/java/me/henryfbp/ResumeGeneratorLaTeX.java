740c356f1e6e50b36b4d5b734eba021ddebdb907
Henry Post <HenryFBP@gmail.com>
2019-11-30 17:52:03 -0500

    start generating latex file

--- src/main/java/me/henryfbp/ResumeGeneratorLaTeX.java
+++ src/main/java/me/henryfbp/ResumeGeneratorLaTeX.java
@@ -4,22 +4,60 @@ package me.henryfbp;
  * Hello world!
  */
 
+import org.apache.commons.io.FilenameUtils;
+
+import java.io.File;
 import java.io.IOException;
 
+import static java.lang.String.format;
+import static me.henryfbp.Constants.DEFAULT_LATEX_RESUME_CLASS_FILE;
 import static me.henryfbp.Constants.RESUME_EXAMPLE;
 
 public class ResumeGeneratorLaTeX {
 
+    public static String makeSingleArgCommand(String command, String arg) {
+        return String.format("\\%s{%s}", command, arg);
+    }
 
-    public static String generateResumeLaTeX(Resume resume) {
-        return "lol";
+    /**
+     * Given a LaTeX class file (*.cls), return the LaTeX expression that imports that class file.
+     */
+    public static String makeDocumentClass(File classFile) {
+        String template = "\\documentclass{%s}%n";
+
+        String extension = FilenameUtils.getExtension(classFile.getName());
+
+        if (!extension.equalsIgnoreCase("cls")) {
+            throw new IllegalArgumentException("Included file must be a .cls file!");
+        }
+
+        return format(template, FilenameUtils.getBaseName(classFile.getName()));
+    }
+
+    public static String makeDocumentHeaders() {
+        return ("\\usepackage[left=0.75in,top=0.6in,right=0.75in,bottom=0.6in]{geometry} %% Document margins\n" +
+                makeSingleArgCommand("usepackage", "hyperref") + "\n" +
+                makeSingleArgCommand("usepackage", "xcolor") + "\n" +
+                "\\newcommand{\\tab}[1]{\\hspace{.2667\\textwidth}\\rlap{#1}}\n" +
+                "\\newcommand{\\itab}[1]{\\hspace{0em}\\rlap{#1}}\n");
+    }
+
+    public static String generateResumeLaTeX(Resume resume, File classFile) {
+        StringBuilder sb = new StringBuilder();
+
+
+        sb.append(("%% Generated from jjsonresume.\n"))
+                .append(makeDocumentClass(classFile))
+                .append(makeDocumentHeaders());
+
+        return sb.toString();
     }
 
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! I make LaTeX resumes!");
 
-        System.out.println(generateResumeLaTeX(RESUME_EXAMPLE));
+        System.out.println(generateResumeLaTeX(RESUME_EXAMPLE, DEFAULT_LATEX_RESUME_CLASS_FILE));
     }
 }
 
